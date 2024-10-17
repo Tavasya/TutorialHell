bd6d2457f36a9d9806d959e0cccc10712ba7922e
Tavasya Ganpati <tavasyag@gmail.com>
2024-10-17 16:59:29 -0700

    initlized app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="title">Escape from <span className="tut">Tutorial</span> <span className="hell">Hell...</span></h1>
+    </>
+  )
+}
+
+export default App  
